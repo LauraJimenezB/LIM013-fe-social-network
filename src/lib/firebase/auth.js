09ef3b35d3b744af0b70fa34e4cadcc0b4b4b266
LauraJimenezB <laura271260@gmail.com>
2020-11-04 13:15:53 -0500

    data object

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -1,4 +1,6 @@
 //Creación de nueva cuenta de usuario
+import {data} from "signUp.js"
+
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
--- src/lib/views/signUp.js
+++ src/lib/views/signUp.js
@@ -8,21 +8,28 @@ const signUpView = `
     <input id="email" type="email" placeholder="Correo">
     <input id="password" type="password" placeholder="Contraseña">
     <input id="confirmPassword" type="password" placeholder="Confirmar contraseña">
-    <button id="mySubmit" class="signButton">Sign Up</button>
 </form>
+<button id="mySubmit" class="signButton">Sign Up</button>
 <a>Ya tienes una cuenta creada?Log in<a>
 `
 document.getElementById("container").innerHTML = signUpView;
 //Obteniendo
+let data={};
 const submitButton = document.getElementById("mySubmit");
 submitButton.addEventListener("click", () => {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     if(document.getElementById("password").value === document.getElementById("confirmPassword").value){
-        let password = document.getElementById("password").value
-        return password;
+        let password = document.getElementById("password").value;
+        data = {
+            user: name, 
+            email: email, 
+            password: password
+            };
+            console.log(data);
+        //return password;
     } else {
         alert("Las contraseñas no coinciden")
     }
-    console.log(name, email, password)
 })
+console.log(data);
