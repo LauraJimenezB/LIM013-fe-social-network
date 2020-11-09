c19a080c2ac4c4d27bfc29826e810cba58167190
Illari Alvarez-Gil <friedapples824qgmail.com>
2020-11-09 09:35:53 -0500

    Configuraciòn npm test, eslint y package

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -1,12 +1,11 @@
-
 const db = firebase.firestore();
-export const  createUserDB = (userUid, email, name, photo) => {
-  db.collection("users").add({
-        name: name,
-        email: email,
-        userUid: userUid,
-        photo: photo,
-    })
-    .then(response => console.log(response))
-    .catch(error => console.log("Hubo un error", error))
-}
+export const createUserDB = (userUid, email, name, photo) => {
+  db.collection('users').add({
+    name,
+    email,
+    userUid,
+    photo,
+  })
+    .then((response) => console.log(response))
+    .catch((error) => console.log('Hubo un error', error));
+};
