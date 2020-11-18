55b3f7bd2c95ac9d0f466ad5d91df962201413ee
LauraJimenezB <laura271260@gmail.com>
2020-11-18 12:40:57 -0500

    responsive, signIn/signUp

--- src/lib/views/signUp.js
+++ src/lib/views/signUp.js
@@ -5,12 +5,10 @@ export const signUp = () => {
   const signUpView = `
   <div class="signUpContainer">
   <header class="signUpHeader">
-    <div class="signUpTitle">
-    </div>
+    <h2 class="h2Up">SIGN UP</h2>
   </header>
   <section class="signUpForm">
     <form>
-      <h2>SIGN UP</h2>
       <input class="signUpInput" id="name" type="text" placeholder="Nombre de usuario">
       <input  class="signUpInput" id="email" type="email" placeholder="Correo">
       <input  class="signUpInput" id="password" type="password" placeholder="Contraseña">
