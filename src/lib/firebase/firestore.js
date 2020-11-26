7f9e0d7f8979adcb132994c2ad3b0af424e60e51
LauraJimenezB <laura271260@gmail.com>
2020-11-26 13:47:31 -0500

    working post buttons

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -30,12 +30,3 @@ export const savePosts = (textValue, photoValue) => {
     .then((response) => console.log(response))
     .catch((error) => console.log(error));
 };
-/*
-export const printPost = () => {
-  db().collection('posts').get()
-    .then((snapshot) => {
-      snapshot.docs.forEach((doc) => showPosts(doc));
-    })
-    .catch((e) => console.log('error', e));
-};
-*/
